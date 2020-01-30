069775bf57d6eadfd39cb50e5b121f41a03f66ed
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-01-30 16:38:59 +0100

    Rename the endpoint to find-username

--- src/main/java/com/joseluisestevez/msa/users/models/entity/dao/UserDao.java
+++ src/main/java/com/joseluisestevez/msa/users/models/entity/dao/UserDao.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.joseluisestevez.msa.users.models.entity.User;
 
@@ -12,6 +13,9 @@ public interface UserDao extends PagingAndSortingRepository<User, Long> {
 
 	// https://docs.spring.io/spring-data/jpa/docs/2.2.4.RELEASE/reference/html/#jpa.query-methods
 	// see Table 3. Supported keywords inside method names Keyword Sample
+	// Por defecto seria /users/search/findByUsername?username=user_one
+	// cambiado a /users/search/find-username?username=user_one
+	@RestResource(path = "find-username")
 	User findByUsername(@Param("username") String username);
 
 	User findByUsernameAndEmail(@Param("username") String username,
