e7df153da7d27aeda02aebbc099768ccf1925a58
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-01-31 08:54:59 +0100

    Use Bcrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (username, password, enabled, firstname, lastname, email) values ('user_one', '123456', true, 'User', 'One', 'user_one@mail.com');
-INSERT INTO users (username, password, enabled, firstname, lastname, email) values ('user_two', '123456', true, 'User', 'Two', 'user_two@mail.com');
+INSERT INTO users (username, password, enabled, firstname, lastname, email) values ('user_one', '$2a$10$WSECdF.aXJHyTeYoFqEtduiHrhhwsLhOaF5BbsVC6g77qWCvmx/US', true, 'User', 'One', 'user_one@mail.com');
+INSERT INTO users (username, password, enabled, firstname, lastname, email) values ('user_two', '$2a$10$Olhpi9EonIgzCZeOWUaL0uXO6WFvEcUE8H4vvc2vBehuDt1DQeaOi', true, 'User', 'Two', 'user_two@mail.com');
 
 INSERT INTO roles (name) VALUES ('ROLE_USER');
 INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
